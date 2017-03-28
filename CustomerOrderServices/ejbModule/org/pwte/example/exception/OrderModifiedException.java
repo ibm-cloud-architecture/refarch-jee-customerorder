45be93c2e60a929f4686a70c9307eae99e8067b2
Rick Osowski <(none)>
2017-03-28 17:26:58 -0500

    Initial commit of original source
    Signed-off-by: Rick Osowski <(none)>

---
+++ CustomerOrderServices/ejbModule/org/pwte/example/exception/OrderModifiedException.java
@@ -0,0 +1,24 @@
+package org.pwte.example.exception;
+
+public class OrderModifiedException extends Exception {
+
+	public OrderModifiedException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public OrderModifiedException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public OrderModifiedException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public OrderModifiedException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ CustomerOrderServices/ejbModule/org/pwte/example/exception/ProductDoesNotExistException.java
@@ -0,0 +1,24 @@
+package org.pwte.example.exception;
+
+public class ProductDoesNotExistException extends Exception {
+
+	public ProductDoesNotExistException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public ProductDoesNotExistException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ProductDoesNotExistException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ProductDoesNotExistException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
