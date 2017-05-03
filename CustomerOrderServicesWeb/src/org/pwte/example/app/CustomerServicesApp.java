f8ceedfcf51caa07e9e251328b128228eb0a9047
vagrant <vagrant@localhost.localdomain>
2017-05-03 14:27:52 +0000

    Update to register JAX-RS application in Web App
    Signed-off-by: vagrant <vagrant@localhost.localdomain>

--- CustomerOrderServicesWeb/src/org/pwte/example/app/CustomerServicesApp.java
+++ CustomerOrderServicesWeb/src/org/pwte/example/app/CustomerServicesApp.java
@@ -1,9 +1,26 @@
 package org.pwte.example.app;
 
-import javax.ws.rs.ApplicationPath;
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.ws.rs.core.Application;
 
-@ApplicationPath("/jaxrs/*")
+import org.pwte.example.resources.CategoryResource;
+import org.pwte.example.resources.CustomerOrderResource;
+import org.pwte.example.resources.ProductResource;
+
 public class CustomerServicesApp extends Application {
 
+	@Override
+	public Set<Class<?>> getClasses() {
+
+		Set<Class<?>> classes = new HashSet<Class<?>>();
+
+		classes.add(CategoryResource.class);
+		classes.add(CustomerOrderResource.class);
+		classes.add(ProductResource.class);
+
+		return classes;
+
+	}
 }
